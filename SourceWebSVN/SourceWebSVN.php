bad53ccdee80db7cf812656385b29d18893c1c15
John Reese <jreese@leetcode.net>
2008-09-08 16:15:22 -0400

    Increment to development version 0.13

--- SourceWebSVN/SourceWebSVN.php
+++ SourceWebSVN/SourceWebSVN.php
@@ -20,10 +20,10 @@ class SourceWebSVNPlugin extends MantisSourcePlugin {
 		$this->name = lang_get( 'plugin_SourceWebSVN_title' );
 		$this->description = lang_get( 'plugin_SourceWebSVN_description' );
 
-		$this->version = '0.12a';
+		$this->version = '0.13';
 		$this->requires = array(
 			'MantisCore' => '1.2.0',
-			'Source' => '0.12a',
+			'Source' => '0.13',
 		);
 
 		$this->author = 'John Reese';
