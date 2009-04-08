371763c468ec82a87b33684178a2e2fdef1edd22
John Reese <jreese@leetcode.net>
2009-04-08 14:03:13 -0400

    Fix #13: Convert project license to AGPL3.

--- Source/Source.API.php
+++ Source/Source.API.php
@@ -1,15 +1,15 @@
 <?php
-# Copyright (C) 2008	John Reese
+# Copyright (C) 2008 John Reese, LeetCode.net
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
+# it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# GNU Affero General Public License for more details.
 
 require_once( 'MantisSourcePlugin.class.php' );
 
