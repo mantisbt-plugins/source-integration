d953a6088bd67c42171e5fdbea56bdc485d1c480
Jose Portoles <anrk547@googlemail.com>
2012-03-29 20:22:04 +0200

    Fixes some issues parsing gitweb, should make it work on 1.7.9
    Eventually someone should rewrite this to parse the atom or rss feed
    on gitweb, but in the meantime this should work.

--- SourceGitweb/SourceGitweb.php
+++ SourceGitweb/SourceGitweb.php
@@ -272,8 +272,7 @@ class SourceGitwebPlugin extends MantisSourcePlugin {
 		if ( !SourceChangeset::exists( $p_repo->id, $t_commit['revision'] ) ) {
 
 			# Parse for commit data
-			preg_match( '#<tr><td>author</td><td>(?:<a[^>]*>)?([^<>]*)(?:</a>)? *(?:<a[^>]*>)?<([^<>]*)>(?:</a>)?</td>(?:<[^<>]*>\s*)*?</tr>'.
-				'<tr><td></td><td> \w*, (\d* \w* \d* \d*:\d*:\d*)#', $t_gitweb_data, $t_matches );
+			preg_match( '#<tr><td>author</td><td>(?:<a[^>]*>)?([^<>]*)(?:</a>)? *(?:<a[^>]*>)?<([^<>]*)>(?:</a>)?</td>(?:<[^<>]*>\s*)*?</tr>\n<tr><td></td><td><span class="datetime">\w*, (\d* \w* \d* \d*:\d*:\d*)#', $t_gitweb_data, $t_matches );
 			$t_commit['author'] = $t_matches[1];
 			$t_commit['author_email'] = $t_matches[2];
 			$t_commit['date'] = date( 'Y-m-d H:i:s', strtotime( $t_matches[3] ) );
@@ -290,7 +289,7 @@ class SourceGitwebPlugin extends MantisSourcePlugin {
 				}
 			}
 
-			preg_match( '#<div class="page_body">(.*)</div>#', $t_gitweb_data, $t_matches );
+			preg_match( '#<div class="page_body">\n(.*)\n</div>#', $t_gitweb_data, $t_matches );
 			$t_commit['message'] = trim( str_replace( '<br/>', PHP_EOL, $t_matches[1] ) );
 
 			# Strip ref links and signoff spans from commit message
@@ -302,8 +301,8 @@ class SourceGitwebPlugin extends MantisSourcePlugin {
 			# Parse for changed file data
 			$t_commit['files'] = array();
 
-			preg_match_all( '#<tr class="(?:light|dark)"><td><a class="list" href="[^"]*;h=(\w+);[^"]*">([^<>]+)</a></td>'.
-				'<td>(?:<span class="file_status (\w+)">[^<>]*</span>)?</td>#',
+			preg_match_all( '#<tr class="(?:light|dark)">\n<td><a class="list" href="[^"]*;h=(\w+);[^"]*">([^<>]+)</a></td>'.
+				'\n<td>(?:<span class="file_status (\w+)">[^<>]*</span>)?</td>#',
 				$t_gitweb_files, $t_matches, PREG_SET_ORDER );
 
 			foreach( $t_matches as $t_file_matches ) {
