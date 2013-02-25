74597a7308cde22ee3ede066cdd8c7d59f4f6f1b
Cyril Margorin <comargo@gmail.com>
2013-02-25 23:42:37 +0400

    Revert "WebSVN: fix broken links"
    This reverts commit 577ff7e9e56bca40d3112a1e1948de1310a32afd, reversing
    changes made to dd09bbda57f723d4ebc9dc4b9c5ddeaeaca5db40.

--- SourceWebSVN/SourceWebSVN.php
+++ SourceWebSVN/SourceWebSVN.php
@@ -30,43 +30,39 @@ class SourceWebSVNPlugin extends SourceSVNPlugin {
 		return lang_get( 'plugin_SourceWebSVN_svn' );
 	}
 
-	protected function websvn_url($p_repo) {
-		$t_path = '';
-		if ( !is_blank( $p_repo->info['websvn_path'] ) ) {
-			$t_path = '/' . urlencode( $p_repo->info['websvn_path'] );
-		}
-		return $p_repo->info['websvn_url'] . $p_repo->info['websvn_name'] . $t_path;
-	}
-
 	public function url_repo( $p_repo, $p_changeset=null ) {
 		$t_rev = '';
+		$t_path = '';
 
 		if ( !is_null( $p_changeset ) ) {
-			$t_rev = '?rev=' . urlencode( $p_changeset->revision );
+			$t_rev = '&rev=' . urlencode( $p_changeset->revision );
 		}
-		return $this->websvn_url($p_repo) . $t_rev;
+		if ( !is_blank( $p_repo->info['websvn_path'] ) ) {
+			$t_path = '&path=' . urlencode( $p_repo->info['websvn_path'] );
+		}
+		return $p_repo->info['websvn_url'] . 'listing.php?repname=' . urlencode( $p_repo->info['websvn_name'] ) . "$t_path$t_rev&sc=1";
 	}
 
 	public function url_changeset( $p_repo, $p_changeset ) {
-		return $this->websvn_url($p_repo) . '?op=comp' .
-			'&compare[]=/@' . urlencode( $p_changeset->revision-1 ) .
-			'&compare[]=/@' . urlencode( $p_changeset->revision);
+		return $this->url_repo( $p_repo, $p_changeset );
 	}
 
 	public function url_file( $p_repo, $p_changeset, $p_file ) {
 		if ( $p_file->action == 'D' ) {
 			return '';
 		}
-		return $this->websvn_url($p_repo) . $p_file->filename . '?op=filedetails' .
-			'&rev=' . urlencode( $p_changeset->revision ) . '&peg=' . urlencode( $p_changeset->revision ) ;
+		return $p_repo->info['websvn_url'] . 'filedetails.php?repname=' . urlencode( $p_repo->info['websvn_name'] ) .
+			'&rev=' . urlencode( $p_changeset->revision ) . '&peg=' . urlencode( $p_changeset->revision ) .
+			'&path=' . urlencode( $p_file->filename ) . '&sc=1';
 	}
 
 	public function url_diff( $p_repo, $p_changeset, $p_file ) {
 		if ( $p_file->action == 'D' || $p_file->action == 'A' ) {
 			return '';
 		}
-		return $this->websvn_url( $p_repo ) . $p_file->filename . '?op=diff' .
-			'&rev=' . urlencode( $p_changeset->revision ) . '&peg=' . urlencode( $p_changeset->revision ) ;
+		return $p_repo->info['websvn_url'] . 'diff.php?repname=' . urlencode( $p_repo->info['websvn_name'] ) .
+			'&rev=' . urlencode( $p_changeset->revision ) . '&peg=' . urlencode( $p_changeset->revision ) .
+			'&path=' . urlencode( $p_file->filename ) . '&sc=1';
 	}
 
 	public function update_repo_form( $p_repo ) {
