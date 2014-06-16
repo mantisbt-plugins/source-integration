912b61c435337ecf0246e74e4b0f4fe27b3c351d
bright-tools <dev@brightsilence.com>
2014-06-16 23:28:09 +0100

    Fix typo in option name

--- Source/pages/repo_update.php
+++ Source/pages/repo_update.php
@@ -16,7 +16,7 @@ $f_repo_commit_status_restricted = gpc_get_bool( 'repo_commit_status_restricted'
 $f_repo_commit_status_allowed = gpc_get_int_array( 'repo_commit_status_allowed', MantisEnum::getValues( config_get( 'status_enum_string' ) ));
 $f_repo_commit_project_restricted = gpc_get_bool( 'repo_commit_project_restricted', false );
 $f_repo_commit_project_allowed = gpc_get_int_array( 'repo_commit_project_allowed', Array( 0 ) );
-$f_repo_commit_committer_must_be_member = gpc_get_bool( 'repo_commit_committer_must_me_member', false );
+$f_repo_commit_committer_must_be_member = gpc_get_bool( 'repo_commit_committer_must_be_member', false );
 $f_repo_commit_committer_must_be_level = gpc_get_int_array( 'repo_commit_committer_must_be_level', MantisEnum::getValues( config_get( 'access_levels_enum_string' ) ));
 
 $t_repo = SourceRepo::load( $f_repo_id );
