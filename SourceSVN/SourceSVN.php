1d2a9488fe96863fdb458405bb27e61ec0ab38d4
sschmerek <s.schmerek@gmail.com>
2015-12-09 14:59:14 +0100

    Update SourceSVN for Mantis 1.3.0

--- SourceSVN/SourceSVN.php
+++ SourceSVN/SourceSVN.php
@@ -14,7 +14,7 @@ class SourceSVNPlugin extends MantisSourcePlugin {
 
 		$this->version = '0.19';
 		$this->requires = array(
-			'MantisCore' => '1.2.0',
+			'MantisCore' => '1.3.0',
 			'Source' => '0.16',
 		);
 
@@ -72,34 +72,55 @@ class SourceSVNPlugin extends MantisSourcePlugin {
 		$t_ignore_paths = isset( $p_repo->info['ignore_paths'] ) ? $p_repo->info['ignore_paths'] : '';
 
 ?>
-<tr>
-<td class="category"><?php echo plugin_lang_get( 'svn_username' ) ?></td>
-<td><input name="svn_username" maxlength="250" size="40" value="<?php echo string_attribute( $t_svn_username ) ?>"/></td>
-</tr>
-<tr>
-<td class="category"><?php echo plugin_lang_get( 'svn_password' ) ?></td>
-<td><input name="svn_password" maxlength="250" size="40" value="<?php echo string_attribute( $t_svn_password ) ?>"/></td>
-</tr>
-<tr>
-<td class="category"><?php echo plugin_lang_get( 'standard_repo' ) ?></td>
-<td><input name="standard_repo" type="checkbox" <?php echo ($t_standard_repo ? 'checked="checked"' : '') ?>/></td>
-</tr>
-<tr>
-<td class="category"><?php echo plugin_lang_get( 'trunk_path' ) ?></td>
-<td><input name="trunk_path" maxlength="250" size="40" value="<?php echo string_attribute( $t_trunk_path ) ?>"/></td>
-</tr>
-<tr>
-<td class="category"><?php echo plugin_lang_get( 'branch_path' ) ?></td>
-<td><input name="branch_path" maxlength="250" size="40" value="<?php echo string_attribute( $t_branch_path ) ?>"/></td>
-</tr>
-<tr>
-<td class="category"><?php echo plugin_lang_get( 'tag_path' ) ?></td>
-<td><input name="tag_path" maxlength="250" size="40" value="<?php echo string_attribute( $t_tag_path ) ?>"/></td>
-</tr>
-<tr>
-<td class="category"><?php echo plugin_lang_get( 'ignore_paths' ) ?></td>
-<td><input name="ignore_paths" type="checkbox" <?php echo ($t_ignore_paths ? 'checked="checked"' : '') ?>/></td>
-</tr>
+<div class="field-container">
+	<label><span><?php echo plugin_lang_get( 'svn_username' ) ?></span></label>
+	<span class="input">
+		<input name="svn_username" maxlength="250" size="40" value="<?php echo string_attribute( $t_svn_username ) ?>"/>
+	</span>
+	<span class="label-style"></span>
+</div>
+<div class="field-container">
+	<label><span><?php echo plugin_lang_get( 'svn_password' ) ?></span></label>
+	<span class="input">
+		<input name="svn_password" maxlength="250" size="40" value="<?php echo string_attribute( $t_svn_password ) ?>"/>
+	</span>
+	<span class="label-style"></span>
+</div>
+<div class="field-container">
+	<label><span><?php echo plugin_lang_get( 'standard_repo' ) ?></span></label>
+	<span class="input">
+		<input name="standard_repo" type="checkbox" <?php echo ($t_standard_repo ? 'checked="checked"' : '') ?>/>
+	</span>
+	<span class="label-style"></span>
+</div>
+<div class="field-container">
+	<label><span><?php echo plugin_lang_get( 'trunk_path' ) ?></span></label>
+	<span class="input">
+		<input name="trunk_path" maxlength="250" size="40" value="<?php echo string_attribute( $t_trunk_path ) ?>"/>
+	</span>
+	<span class="label-style"></span>
+</div>
+<div class="field-container">
+	<label><span><?php echo plugin_lang_get( 'branch_path' ) ?></span></label>
+	<span class="input">
+		<input name="branch_path" maxlength="250" size="40" value="<?php echo string_attribute( $t_branch_path ) ?>"/>
+	</span>
+	<span class="label-style"></span>
+</div>
+<div class="field-container">
+	<label><span><?php echo plugin_lang_get( 'tag_path' ) ?></span></label>
+	<span class="input">
+		<input name="tag_path" maxlength="250" size="40" value="<?php echo string_attribute( $t_tag_path ) ?>"/>
+	</span>
+	<span class="label-style"></span>
+</div>
+<div class="field-container">
+	<label><span><?php echo plugin_lang_get( 'ignore_paths' ) ?></span></label>
+	<span class="input">
+		<input name="ignore_paths" type="checkbox" <?php echo ($t_ignore_paths ? 'checked="checked"' : '') ?>/>
+	</span>
+	<span class="label-style"></span>
+</div>
 <?php
 	}
 
