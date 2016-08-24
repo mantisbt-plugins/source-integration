01e3b46d41993c6751b8022e9d782ff2329984ab
root <root@euve41889.serverprofi24.de>
2016-08-24 20:14:00 +0200

    Remove calls to deprecated helper_alternate_class()

--- SourceGitlab/SourceGitlab.php
+++ SourceGitlab/SourceGitlab.php
@@ -19,10 +19,10 @@ class SourceGitlabPlugin extends MantisSourcePlugin {
 		$this->name = plugin_lang_get( 'title' );
 		$this->description = plugin_lang_get( 'description' );
 
-		$this->version = '1.0.3';
+		$this->version = '1.0.4';
 		$this->requires = array(
 			'MantisCore' => '1.3.0',
-			'Source' => '0.16',
+			'Source' => '1.3.0',
 		);
 
 		$this->author = 'Johannes Goehr';
@@ -120,23 +120,23 @@ public function update_repo_form( $p_repo ) {
 			$t_master_branch = 'master';
 		}
 ?>
-<tr <?php echo helper_alternate_class() ?>>
+<tr>
 <td class="category"><?php echo plugin_lang_get( 'hub_root' ) ?></td>
 <td><input name="hub_root" maxlength="250" size="40" value="<?php echo string_attribute( $t_hub_root ) ?>"/></td>
 </tr>
-<tr <?php echo helper_alternate_class() ?>>
+<tr>
 <td class="category"><?php echo plugin_lang_get( 'hub_repoid' ) ?></td>
 <td><input name="hub_repoid" maxlength="250" size="40" value="<?php echo string_attribute( $t_hub_repoid ) ?>"/></td>
 </tr>
-<tr <?php echo helper_alternate_class() ?>>
+<tr>
 <td class="category"><?php echo plugin_lang_get( 'hub_reponame' ) ?></td>
 <td><input name="hub_reponame" maxlength="250" size="40" value="<?php echo string_attribute( $t_hub_reponame ) ?>"/></td>
 </tr>
-<tr <?php echo helper_alternate_class() ?>>
+<tr>
 <td class="category"><?php echo plugin_lang_get( 'hub_app_secret' ) ?></td>
 <td><input name="hub_app_secret" maxlength="250" size="40" value="<?php echo string_attribute( $t_hub_app_secret ) ?>"/></td>
 </tr>
-<tr <?php echo helper_alternate_class() ?>>
+<tr>
 <td class="category"><?php echo plugin_lang_get( 'master_branch' ) ?></td>
 <td><input name="master_branch" maxlength="250" size="40" value="<?php echo string_attribute( $t_master_branch ) ?>"/></td>
 </tr>
