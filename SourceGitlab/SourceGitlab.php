mantisbt-plugins/source-integration

commit bfb862f758fedff870d55352dd91b10c4ae58065

Fix primary branch validation regex
Prior to this, a '*' anywhere in the field would cause input to be
accepted even in the presence of other invalid characters.

SourceGitlab/SourceGitlab.php
@@ -94,7 +94,7 @@ class SourceGitlabPlugin extends MantisSourcePlugin {
 		$t_ref = $p_changeset->revision;
 		$t_filename = $p_file->filename;
 
-		return "$t_root/$t_reponame/commit/$t_ref?view=parallel";
+		return "t_root/$t_reponame/commit/$t_ref?view=parallel";
 	}
 
 public function update_repo_form( $p_repo ) {
@@ -171,7 +171,7 @@ public function update_repo_form( $p_repo ) {
 		$f_hub_app_secret = gpc_get_string( 'hub_app_secret' );
 		$f_master_branch = gpc_get_string( 'master_branch' );
 
-		if ( !preg_match( '/\*|^[a-zA-Z0-9_\., -]*$/', $f_master_branch ) ) {
+		if ( !preg_match( '/^(\*|[a-zA-Z0-9_\., -]*)$/', $f_master_branch ) ) {
 			plugin_error( self::ERROR_INVALID_PRIMARY_BRANCH );
 		}
 

SourceGithub/SourceGithub.php
@@ -171,7 +171,7 @@ endif; ?></td>
 		$f_hub_app_secret = gpc_get_string( 'hub_app_secret' );
 		$f_master_branch = gpc_get_string( 'master_branch' );
 
-		if ( !preg_match( '/\*|^[a-zA-Z0-9_\., -]*$/', $f_master_branch ) ) {
+		if ( !preg_match( '/^(\*|[a-zA-Z0-9_\., -]*)$/', $f_master_branch ) ) {
 			plugin_error( self::ERROR_INVALID_PRIMARY_BRANCH );
 		}
 

commit 287a4134ce8b461855b9969e624c6d494a97eb9e

Correct Gitlab diff URL
Regression introduced in bfb862f758fedff870d55352dd91b10c4ae58065

Fixes #226

SourceGitlab/SourceGitlab.php
@@ -85,7 +85,7 @@ class SourceGitlabPlugin extends MantisSourceGitBasePlugin {
 		$t_ref = $p_changeset->revision;
 		$t_filename = $p_file->filename;
 
-		return "t_root/$t_reponame/commit/$t_ref?view=parallel";
+		return "$t_root/$t_reponame/commit/$t_ref?view=parallel";
 	}
 
 public function update_repo_form( $p_repo ) {