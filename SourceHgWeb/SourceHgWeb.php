d0b8d00bf2b234deeeeff090007b942a5d1980a2
Damien Regad <dregad@mantisbt.org>
2018-05-11 11:02:17 +0200

    Bump version and update changelog

--- SourceHgWeb/SourceHgWeb.php
+++ SourceHgWeb/SourceHgWeb.php
@@ -19,7 +19,7 @@ require_once( config_get( 'core_path' ) . 'url_api.php' );
 
 class SourceHgWebPlugin extends MantisSourcePlugin {
 
-	const PLUGIN_VERSION = '2.1.1';
+	const PLUGIN_VERSION = '2.1.2';
 	const FRAMEWORK_VERSION_REQUIRED = '2.0.0';
 
 	/**
