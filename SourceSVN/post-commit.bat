9bbd4ce3a95eb523095eefc7b49c1e27645cbf35
Damien Regad <dregad@mantisbt.org>
2020-02-07 23:20:50 +0100

    Avoid unwanted output during .bat script execution
    Moving the REM statement after @echo off ensures it is not displayed
    when the script runs.

--- SourceSVN/post-commit.bat
+++ SourceSVN/post-commit.bat
@@ -1,5 +1,5 @@
-REM Command line wrapper to start PowerShell hook script
 @echo off
+REM Command line wrapper to start PowerShell hook script
 set PWSH=%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe
 %PWSH% -ExecutionPolicy Bypass -NonInteractive -NoProfile -Command "$input | %1\hooks\post-commit.ps1" "%1" "%2" "%3"
 if errorlevel 1 exit /b %errorlevel%
